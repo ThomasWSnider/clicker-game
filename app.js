cbc0e404437f31dd538b85d9883dd24dfee081ad
Thomas Snider <thomas.wayne.snider@gmail.com>
2024-06-14 17:23:01 -0600

    Added cart upgrade

--- app.js
+++ app.js
@@ -24,44 +24,54 @@ const autoUpgrades = [
   },
 ]
 
-function buyShovel() {
-  const clickUpgrade = clickUpgrades.find((upgrade) => upgrade.name == 'shovel')
-  if (treasure >= clickUpgrade.price) {
-    treasure -= clickUpgrade.price
-    clickUpgrade.price = clickUpgrade.price * 2
-    clickUpgrade.quantity++
-    const shovelPriceElement = document.getElementById('shovelPrice')
-    shovelPriceElement.innerText = `🪙${clickUpgrade.price}`
-    const shovelQuantityElement = document.getElementById('shovelQuantity')
-    shovelQuantityElement.innerText = `${clickUpgrade.quantity}`
-  }
-  else {
-    return
-  }
-  drawTreasure()
-}
+// function buyShovel() {
+//   const clickUpgrade = clickUpgrades.find((upgrade) => upgrade.name == 'shovel')
+//   if (treasure >= clickUpgrade.price) {
+//     treasure -= clickUpgrade.price
+//     clickUpgrade.price = clickUpgrade.price * 2
+//     clickUpgrade.quantity++
+//     const shovelPriceElement = document.getElementById('shovelPrice')
+//     shovelPriceElement.innerText = `🪙${clickUpgrade.price}`
+//     const shovelQuantityElement = document.getElementById('shovelQuantity')
+//     shovelQuantityElement.innerText = `${clickUpgrade.quantity}`
+//   }
+//   else {
+//     return
+//   }
+//   drawTreasure()
+// }
 
 function buyClickUpgrade(upgradeName) {
   const clickUpgrade = clickUpgrades.find((upgrade) => upgrade.name == upgradeName)
-  if (tr)
-}
-
-function buyCart() {
-  const clickUpgrade = clickUpgrades.find((upgrade) => upgrade.name == 'cart')
   if (treasure >= clickUpgrade.price) {
     treasure -= clickUpgrade.price
     clickUpgrade.price = clickUpgrade.price * 2
     clickUpgrade.quantity++
-    const shovelPriceElement = document.getElementById('cartPrice')
-    shovelPriceElement.innerText = `🪙${clickUpgrade.price}`
-    const shovelQuantityElement = document.getElementById('cartQuantity')
-    shovelQuantityElement.innerText = `${clickUpgrade.quantity}`
+
+    drawUpgrades(upgradeName)
+    drawTreasure()
   }
-  else {
-    return
-  }
-  drawTreasure()
 }
+
+
+
+
+// function buyCart() {
+//   const clickUpgrade = clickUpgrades.find((upgrade) => upgrade.name == 'cart')
+//   if (treasure >= clickUpgrade.price) {
+//     treasure -= clickUpgrade.price
+//     clickUpgrade.price = clickUpgrade.price * 2
+//     clickUpgrade.quantity++
+//     const shovelPriceElement = document.getElementById('cartPrice')
+//     shovelPriceElement.innerText = `🪙${clickUpgrade.price}`
+//     const shovelQuantityElement = document.getElementById('cartQuantity')
+//     shovelQuantityElement.innerText = `${clickUpgrade.quantity}`
+//   }
+//   else {
+//     return
+//   }
+//   drawTreasure()
+// }
 
 function buyUnseenServant() {
   const autoUpgrade = autoUpgrades.find((upgrade) => upgrade.name == 'unseenServant')
@@ -83,12 +93,10 @@ function buyUnseenServant() {
 }
 
 function plunder() {
+  treasure++
   clickUpgrades.forEach((upgrade) => {
     if (upgrade.quantity > 0) {
-      treasure += 1 + (upgrade.quantity * upgrade.multiplier)
-    }
-    else {
-      treasure++
+      treasure += upgrade.quantity * upgrade.multiplier
     }
   })
   drawTreasure()
@@ -106,6 +114,14 @@ function collectAutoUpgrades() {
 function drawTreasure() {
   const treasureElement = document.getElementById('treasure')
   treasureElement.innerText = treasure.toString()
+}
+
+function drawUpgrades(upgradeName) {
+  const clickUpgrade = clickUpgrades.find((upgrade) => upgrade.name == upgradeName)
+  const upgradePriceElement = document.getElementById(`${upgradeName}Price`)
+  upgradePriceElement.innerText = `🪙${clickUpgrade.price}`
+  const upgradeQuantityElement = document.getElementById(`${upgradeName}Quantity`)
+  upgradeQuantityElement.innerText = `${clickUpgrade.quantity}`
 }
 
 function getFunds() {
